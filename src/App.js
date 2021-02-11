cf4cef88231137fda88da2347508ae8985cdc40d
Andrew Tolochkevych <andriy410tolochkevitch@gmail.com>
2021-02-11 19:47:59 +0200

    remove redux-devtols-extension

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import "./App.css"
 import React from "react"
-
 import Home from "./components/pages/Home"
 
-function App() {
-  return (
-    <Home />
-    // <BrowserRouter>
-    //   <NavComponent />
-    //   <Switch>
-    //     <Route path={"/"} exact component={Home} />
-    //     <Route path={"/converter"} component={Converter} />
-    //   </Switch>
-    // </BrowserRouter>
-  )
+export default function App() {
+  return <Home />
 }
-
-export default App
